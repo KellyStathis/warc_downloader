908ecafbb334c0af9a3b83128e2ae291401726a7
Kelly Stathis <kelly.stathis@gmail.com>
2019-08-28 21:14:51 -0700

    - Read list of crawl_nums from a text file, crawl_nums.txt - archiveit.env renamed to credentials.env - try/except around get request for metadata file - Prompt for collection number and save metadata files to a subdirectory for that collection - Set limits from /reports endpoint calls to 1,000,000

--- metadata_downloader.py
+++ metadata_downloader.py
@@ -10,42 +10,59 @@ import requests
 import os
 archive_it_user = ""
 archive_it_pw = ""
-env_file = "archiveit.env"
+env_file = "credentials.env"
 env_vars = {}
+crawl_nums_file = "crawl_nums.txt"
+from colorama import Fore
 
 # file download function
 def download_metadata_file(url, crawl_num):
     r = requests.get(url, auth=(archive_it_user, archive_it_pw))
     # Write downloaded file
-    filename = r.headers.get('content-disposition').split("filename=")[1] \
-                        .split("\"")[1].replace(":", "_")
-    print('\nDownloading ' + filename + "...")
-    with open(os.getcwd() + '/' + filename, 'wb') as f:  
-        f.write(r.content)
+    try:
+        filename = r.headers.get('content-disposition').split("filename=")[1] \
+                            .split("\"")[1].replace(":", "_")
+        print('\nDownloading ' + filename + "...")
+        with open(os.getcwd() + '/' + filename, 'wb') as f:  
+            f.write(r.content)
+    except:
+        print(Fore.RED + "\nIMPORTANT: Metadata file not found at url: " + url)
+        print(Fore.RESET)
 
- # Get username and password from archiveit.env file
+ # Get username and password from credentials.env file
 with open(env_file) as f:
     for line in f:
         if line.startswith('#'):
             continue
         key, value = line.strip().split('=', 1)
-        # os.environ[key] = value  # Load to local environ
-        #env_vars.append({'name': key, 'value': value}) # Save to a list
         env_vars[key] = value
 
 archive_it_user = env_vars['ARCHIVE-IT-USER']
 archive_it_pw = env_vars['ARCHIVE-IT-PWD']
+
+crawl_nums = []
+with open(crawl_nums_file) as f:
+    for line in f:
+        crawl_nums.append(int(line))
         
-crawl_nums = [88693, 153407, 153606]
+
+dir_name = "crawl_metadata_" + input("collection number: ")
+
+try:       
+    os.mkdir(dir_name)
+except:
+    pass
+
+os.chdir(dir_name)
  
 for crawl_num in crawl_nums:       
     # Download seed, host, and mimetype lists
     seed_list_url = ('https://partner.archive-it.org/api/reports/seed/' 
-                     + str(crawl_num) + '?format=csv&offset=0&limit=1')
+                     + str(crawl_num) + '?format=csv&limit=1000000')
     host_list_url = ('https://partner.archive-it.org/api/reports/host/' 
-                     + str(crawl_num) + '?format=csv&offset=0&limit=3')
+                     + str(crawl_num) + '?format=csv&limit=1000000')
     mimetype_list_url = ('https://partner.archive-it.org/api/reports/mimetype/' 
-                         + str(crawl_num) + '?format=csv&offset=0&limit=3')
+                         + str(crawl_num) + '?format=csv&limit=1000000')
        
     download_metadata_file(seed_list_url, crawl_num)
     download_metadata_file(host_list_url, crawl_num)
